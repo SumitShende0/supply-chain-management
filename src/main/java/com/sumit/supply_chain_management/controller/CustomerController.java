98a21e25a8061db04b8ffdee014a4bcb745557a6
Sumit Shende <sumitshende535@gmail.com>
2025-04-05 19:30:27 +0530

    create endpoint for register and place order

--- src/main/java/com/sumit/supply_chain_management/controller/CustomerController.java
+++ src/main/java/com/sumit/supply_chain_management/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.sumit.supply_chain_management.controller;
 
 import com.sumit.supply_chain_management.model.Customer;
+import com.sumit.supply_chain_management.model.Order;
 import com.sumit.supply_chain_management.service.CustomerSerevice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/customer")
 public class CustomerController {
 
     @Autowired
@@ -21,6 +22,13 @@ public class CustomerController {
     public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer){
         Customer savedCustomer = service.registerCustomer(customer);
         return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/order")
+    public  ResponseEntity<Order> placeOrder(@RequestBody Order order){
+        Order placedOrder = service.placeOrder(order);
+        return  new ResponseEntity<>(placedOrder, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/sumit/supply_chain_management/service/CustomerSerevice.java
+++ src/main/java/com/sumit/supply_chain_management/service/CustomerSerevice.java
@@ -1,6 +1,7 @@
 package com.sumit.supply_chain_management.service;
 
 import com.sumit.supply_chain_management.model.Customer;
+import com.sumit.supply_chain_management.model.Order;
 import com.sumit.supply_chain_management.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +16,7 @@ public class CustomerSerevice {
     }
 
 
+    public Order placeOrder(Order order) {
+         return  repo.save(order);
+    }
 }
